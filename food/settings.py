811551593073d32fcb31db394d49840f63a6c2b9
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-06-24 13:19:11 +0100

    add in the models for teh blog posts

--- food/settings.py
+++ food/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tdr_*g1n*)u#g0af^^y)p^(e+pq7vyub0s^m*hbsxj7bcjpn&^'
+#SECRET_KEY = 'django-insecure-tdr_*g1n*)u#g0af^^y)p^(e+pq7vyub0s^m*hbsxj7bcjpn&^'
+SECRET_KEY = os.environ.get("=b_uk[]ncWm}T@W")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
