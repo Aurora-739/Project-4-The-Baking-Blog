059e4e3840b17f9fb58064406d33dc0b04e40d47
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-06-27 17:12:08 +0100

    style the comment boxes, p

--- food/settings.py
+++ food/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "=b_uk[]ncWm}T@W"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
