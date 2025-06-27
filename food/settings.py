3dd5c8e455fd664ec0977d5257319c4edf81f05f
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-06-27 09:47:56 +0100

    push to heroku

--- food/settings.py
+++ food/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = "=b_uk[]ncWm}T@W"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
