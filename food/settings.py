ab4237aefbbf131f29c59c7d940636c03f370100
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-10-23 19:31:14 +0100

    add whitenoise to settings.py for deployment

--- food/settings.py
+++ food/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'django.contrib.sites',
+    'whitenoise.runserver_nostatic',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
